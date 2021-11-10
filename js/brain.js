5f7b37da4713d1e53da0f562700e5ad5bd749864
NXN-pro <69213469+NXN-pro@users.noreply.github.com>
2021-11-10 21:18:45 +0600

    gsfdsfgwtd

---
+++ js/brain.js
@@ -0,0 +1,11 @@
+
+let user = "Nirjon";
+
+let x = "";
+
+function show(){
+	user = "hsgdghd";
+    x = x + "<br>" +  "[ "+user +" ]  "+document.getElementById("chatBox").value;
+
+    document.getElementById("chat").innerHTML =  x;
+}
